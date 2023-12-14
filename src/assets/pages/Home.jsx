d2e72140fff135d50c175cb1db7fd500fc65d6a0
vijendra2244 <vs230267@gmail.com>
2023-12-14 09:26:39 +0530

    Updated portfolio

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./Home.module.css";
+import myImage from "../../assets/VijendraChouhan.png";
+import myResume from "../../assets/Vijendra-Chouhan-Resume.pdf";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+function Home() {
+  return (
+    <>
+      <div id="home_section" className={styles.containerOfHomeSection}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={myImage} />
+        </div>
+        <div className={styles.detailsContainer}>
+          <h1 className={styles.headingName}>
+            <span className={styles.nameOfAdmin}>I'M VIJENDRA CHOUHAN.</span>{" "}
+            <br /> FRONT-END DEVELOPER
+          </h1>
+
+          <button className={styles.resumeButton}>
+            <a
+              className={styles.resumeAnchor}
+              href={myResume}
+              onClick={() => {
+                window.open(
+                  "https://drive.google.com/file/d/10vMNNYP0-y2tlyPDV9QTI2MjM29iCcj6/view?usp=sharing"
+                );
+              }}
+              target="_blank"
+              download
+            >
+              Resume
+            </a>
+          </button>
+          <div className={styles.accountOfSocialMedia}>
+            <a href="https://github.com/Vijendra2244" target="_blank">
+              <button className={styles.socialMediaIcon}>
+                <FaGithub />
+              </button>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/vijendra-chouhan-989687292/"
+              target="_blank"
+            >
+              <button className={styles.socialMediaIcon}>
+                <FaLinkedin />
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/section/Navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import AllRoute from "../../assets/AllRoutes/AllRoute";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+import { FaHome, FaUser } from "react-icons/fa";
+import { IoMdContact } from "react-icons/io";
+import { GiSkills } from "react-icons/gi";
+import { GoProjectSymlink } from "react-icons/go";
+import resume from "../../assets/Vijendra-Chouhan-Resume.pdf";
+import { RiBookReadLine } from "react-icons/ri";
+import About from "../../assets/pages/About";
+function Navbar() {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenuBtn = () => {
+    setMenu(true);
+  };
+  const handleCross = () => {
+    setMenu(false);
+  };
+  return (
+    <>
+      <div className={styles.mainOfNavbar}>
+        <div>
+          <span className={styles.adminName}>Vijendra</span>
+        </div>
+        <div
+          className={
+            menu ? styles.listOfSectionAfterClick : styles.listOfSection
+          }
+        >
+          <a className={styles.listSectionNavbar} href="#home_section">
+            Home <FaHome />
+          </a>
+          <a className={styles.listSectionNavbar} href="#about_section">
+            About <FaUser />
+          </a>
+
+          <a className={styles.listSectionNavbar} href="#skills_section">
+            Skills <GiSkills />
+          </a>
+          <a className={styles.listSectionNavbar} href="#project_section">
+            Project <GoProjectSymlink />
+          </a>
+          <a className={styles.listSectionNavbar} href="#contact_section">
+            Contact <IoMdContact />
+          </a>
+          <a
+            className={styles.listSectionNavbar}
+            href={resume}
+            onClick={() => {
+              window.open(
+                "https://drive.google.com/file/d/10vMNNYP0-y2tlyPDV9QTI2MjM29iCcj6/view?usp=sharing"
+              );
+            }}
+            target="_blank"
+            download="Vijendra-Chouhan-Resume"
+          >
+            Resume <RiBookReadLine />
+          </a>
+        </div>
+        {menu ? (
+          <div className={styles.crossIcon}>
+            <ImCross onClick={handleCross}></ImCross>
+          </div>
+        ) : (
+          <div className={styles.hamburgerMenuICon}>
+            <GiHamburgerMenu onClick={handleMenuBtn}></GiHamburgerMenu>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
