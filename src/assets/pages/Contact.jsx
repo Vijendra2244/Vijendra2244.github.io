b72cc3c1e2924e1e3e04b8446f1770012725ce5a
vijendra2244 <vs230267@gmail.com>
2024-03-01 13:45:08 +0530

    New

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -8,33 +8,6 @@ import ContactCard from "../../components/ContactCard";
 function Contact() {
   return (
     <div id="contact_section">
-      {/* <h1 className={styles.conatactHeading}>Contact me</h1>
-      <div className={styles.detailOfContact}>
-        <a
-          href="https://www.linkedin.com/in/vijendra-chouhan-989687292/"
-          target="_blank"
-          className={styles.adminDetailsAccount}
-        >
-          <FaLinkedin /> Linkedin
-        </a>
-        <a
-          className={styles.adminDetailsAccount}
-          href="https://github.com/Vijendra2244"
-          target="_blank"
-        >
-          <FaGithubSquare /> Github
-        </a>
-        <a className={styles.adminDetailsAccount}>
-          <IoMdMailUnread /> vs230267@gmail.com
-        </a>
-        <a className={styles.adminDetailsAccount} >
-          <PiPhoneCallFill /> +919009060724
-        </a>
-      </div>
-      <p className={styles.coprRightSection}>
-        Designed And Built By <span className={styles.adminName}>Vijendra</span>{" "}
-        &copy; 2023
-      </p> */}
       <ContactCard />
     </div>
   );
